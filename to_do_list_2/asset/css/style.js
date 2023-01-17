1e369196ba8f88a9ce82fc28921c6be12e7f4fc8
Emily Saito <99355109+Saitocrimson@users.noreply.github.com>
2023-01-17 19:47:45 -0300

    Create style.js

---
+++ to_do_list_2/asset/css/style.js
@@ -0,0 +1,59 @@
+body{
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+
+}
+#conjuntoList{
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+}
+/*#conjuntoEsq{
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 200px;
+border: 2px solid crimson;
+margin:20px;
+}*/
+#newI{
+width: 470px;
+}
+#caixa{
+display: flex;
+flex-direction: row;
+}
+#listatodo{
+display: flex;
+flex-direction: column;
+width: 520px;
+border:2px solid azure;
+margin: 10px;
+}
+#configuracao{
+width: 250px;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+}
+#centralizform{
+visibility: hidden;
+}
+.lado{
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+align-items:center;
+width: 200px;
+height: 100px;
+}
+h1{
+display: flex;
+justify-content:center;
+align-items:center;
+margin:0;
+}
